d080b3353c9b67d07cfd203149d715e1e03088ed
yexxjx <chenstyle02@gmail.com>
2025-12-24 18:24:31 +0900

    251224 오후진짜코드

---
+++ day02/practice2.js
@@ -0,0 +1,37 @@
+
+
+// 숫자 찾기
+console.log(2000) //제품별가격
+console.log(4000) //총결제금액
+console.log(3) // 주문수량
+// vs 가독성 높이기 위해 변수 활용
+let 제품별가격=2000
+console.log(제품별가격);
+let 총결제금액=4000
+console.log(총결제금액);
+let 주문수량=3
+console.log(주문수량);
+
+// 2. 문자열 찾기
+console.log("아메리카노", "커피", "스무디/쉐이크", "A-03")
+
+// 3. 불리언 찾기
+const 품절여부 = true; console.log(품절여부);
+const 영수증출력여부 = false; console.log(영수증출력여부);
+const 적립여부 = true;console.log(적립여부);
+
+// 4. 배열 찾기
+let priceary=[2000,3000,4000]
+console.log(priceary)
+let categoryary=["커피","음료","주스"]
+console.log(categoryary)
+let productary=["아메리카노","바닐라라떼","카페모카"]
+console.log(productary)
+
+// 5. 객체 찾기
+let productobj ={name:"원조커피", price:2500, stock:35}
+console.log(productobj)
+let receiptobj ={ordernumber:"A-03", price:3500, day:"2025-12-24"}
+console.log(receiptobj)
+let cafeobj ={brand:"빽다방", location:"성결대점", open:"09:30"}
+console.log(cafeobj)
---
+++ day02/practice3.js
@@ -0,0 +1,3 @@
+let name =prompt("이름을 입력하세요,")
+let drink =prompt("원하시는 음료를 선택하세요.")
+let stock =prompt("주문 수량을 입력하세요.")
