0279ab9016e9bfc9118594952cd4d72cf7bc9a4a
yexxjx <chenstyle02@gmail.com>
2025-12-25 15:25:53 +0900

    251225 과제3 최종

--- day02/practice3-1.js
+++ day02/practice3-1.js
@@ -1,11 +1,11 @@
-let name=prompt("이름")
-let drink=prompt("원하는 음료")
-let stock=prompt("주문 수량")
+let 이름=prompt("이름")
+let 음료=prompt("원하는 음료")
+let 수량=prompt("주문 수량")
 
 console.log("[주문 접수]");
 console.log("이름");
-console.log(name);
+console.log(이름);
 console.log("메뉴");
-console.log(drink);
+console.log(음료);
 console.log("수량");
-console.log(stock);
+console.log(수량);
--- day02/practice3-2.js
+++ day02/practice3-2.js
@@ -1,13 +1,13 @@
-let name=prompt("이름")
-let type=prompt("운동 종류")
-let time=prompt("[운동 시간(분 단위)]")
+let 이름=prompt("이름")
+let 종류=prompt("운동 종류")
+let 시간=prompt("[운동 시간(분 단위)]")
 
 console.log("[운동 기록]");
 console.log("이름");
-console.log(name);
+console.log(이름);
 console.log("운동 종류");
-console.log(type);
+console.log(종류);
 console.log("운동 시간");
-console.log(time);
+console.log(시간);
 console.log("칼로리");
-console.log(time*7);
+console.log(시간*7);
