e25a7ae3de0cc527b77775ed46db879291af5aa0
yexxjx <chenstyle02@gmail.com>
2025-12-25 15:22:22 +0900

    251225 과제3

---
+++ day02/practice3-1.js
@@ -0,0 +1,11 @@
+let name=prompt("이름")
+let drink=prompt("원하는 음료")
+let stock=prompt("주문 수량")
+
+console.log("[주문 접수]");
+console.log("이름");
+console.log(name);
+console.log("메뉴");
+console.log(drink);
+console.log("수량");
+console.log(stock);
---
+++ day02/practice3-2.js
@@ -0,0 +1,13 @@
+let name=prompt("이름")
+let type=prompt("운동 종류")
+let time=prompt("[운동 시간(분 단위)]")
+
+console.log("[운동 기록]");
+console.log("이름");
+console.log(name);
+console.log("운동 종류");
+console.log(type);
+console.log("운동 시간");
+console.log(time);
+console.log("칼로리");
+console.log(time*7);
